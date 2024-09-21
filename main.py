9d69caa34744a5cde1d151378cd09240dcd49775
Eldar Khanmagomedov <bratiya234@gmail.com>
2024-09-21 10:46:08 +0300

    Рабочий вариант бота угадай число без базы игроков

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def random_number() -> int:
 
 @dp.message(CommandStart())
 async def start_command(message: Message):
-    await message.answer("Hay! Go to game!")
+    await message.answer("Начнем играть?")
 
 @dp.message(Command(commands=["help"]))
 async def help_command(message: Message):
-    await message.answer("Hey! You ara a idiot")
+    await message.answer("Вы должны угадать какое число я загадал от 1 до 100")
 
 @dp.message(Command(commands=["score"]))
 async def start_commands(message: Message):
@@ -47,9 +47,51 @@ async def cancel_command(message: Message):
 
 @dp.message(F.text.lower().in_(["да", "давай", "го", "согласен", "начнем"]))
 async def start_game(message: Message):
-    if user["in_game"] == False:
+    if not user["in_game"]:
         user["in_game"] = True
+        user["secret_number"] = random_number()
+        user['attemps'] = ATTEMPTS
+        await message.answer("Игра начинается я загадал число от 1 до 100")
+    else:
+        await message.answer("Мы пока играем если хотите закончить напишите /cancel")
 
+@dp.message(F.text.lower().in_(["нет", "не хочу", "не"]))
+async def not_start_game(message: Message):
+    if not user["in_game"]:
+        await message.answer("Жаль но ладно")
+    else:
+        await message.answer("Мы сейчас играем")
+
+@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
+async def game(message: Message):
+    if user["in_game"]:
+        if int(message.text) == user["secret_number"]:
+            user["in_game"] = False
+            user["total_game"] += 1
+            user["win"] += 1
+            await message.answer("You Win!")
+        elif int(message.text) > user["secret_number"]:
+            user['attemps'] -= 1
+            await message.answer("Мое чило меньше")
+        elif int(message.text) < user["secret_number"]:
+            user['attemps'] -= 1
+            await message.answer("Мое чило больше")
+
+        if user["attemps"] == 0:
+            user["in_game"] = False
+            user["total_game"] += 1
+            await message.answer("You Lose!")
+    else:
+        await message.answer("Мы еще не играем")
+
+
+
+@dp.message()
+async def message_our(message: Message):
+    if user["in_game"]:
+        await message.answer("Мы сейчас играем пришите числа")
+    else:
+        await message.answer("Если хотите сыграть напишите Да")
 
 if __name__ == "__main__":
     dp.run_polling(bot)
