c7ff0811bc932f8798092c480a9cebd42204c889
Eldar Khanmagomedov <bratiya234@gmail.com>
2024-09-30 14:13:11 +0300

    Теперь если писать маты с пробелами бот все равно их находит а также удалил файл botFilter.py а код написал в файле main.py

--- main.py
+++ main.py
@@ -1,97 +1,99 @@
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command, CommandStart
+from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram import F
 
-import random
-
-BOT_TOKEN = "7202593170:AAExJYXkm_2PzQnT1POLLzfhpXBruvw1N4U"
+BOT_TOKEN = "7921078426:AAGE9AzemlTU6XeiopTCot18tudIk27IyAg"
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-ATTEMPTS = 5
+banworld = open("banword.txt", encoding='utf-8')
+banworld_list = banworld.readlines()
 
-user = {
-    "in_game": False,
-    "secret_number": None,
-    "attemps": None,
-    "total_game": 0,
-    "win": 0
-}
+admin_name = open("ADMIN_NAME", encoding='utf-8')
+admin_name_list = admin_name.readlines()
 
-def random_number() -> int:
-    return random.randint(0, 100)
+ban_user_name = open("BAN_USER_NAME", encoding='utf-8')
+ban_user_name_list = ban_user_name.readlines()
 
+MAX_MATS = 5
 
-@dp.message(CommandStart())
-async def start_command(message: Message):
-    await message.answer("Начнем играть?")
+users = {}
 
-@dp.message(Command(commands=["help"]))
-async def help_command(message: Message):
-    await message.answer("Вы должны угадать какое число я загадал от 1 до 100")
-
-@dp.message(Command(commands=["score"]))
-async def start_commands(message: Message):
-    await message.answer(f'Всего игр сыгранно: {user["total_game"]} \n'
-                         f'Выйгранных игр: {user["win"]}')
-
-@dp.message(Command(commands=["cancel"]))
-async def cancel_command(message: Message):
-    if user["in_game"] == True:
-        user["in_game"] = False
-        await message.answer("Вы вышли из игры")
-    else:
-        await message.answer("Мы с вами не играем")
+#Просто выдаем сколько предупреждений у пользователя
+@dp.message(Command(commands=["mats"]))
+async def mats_command(message: Message):
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            "name": message.from_user.first_name,
+            "mats": 0,
+            "is_ban": False
+        }
+    await message.answer(f"Число пердупреждений: {users[message.from_user.id]['mats']}")
 
 
-@dp.message(F.text.lower().in_(["да", "давай", "го", "согласен", "начнем"]))
-async def start_game(message: Message):
-    if not user["in_game"]:
-        user["in_game"] = True
-        user["secret_number"] = random_number()
-        user['attemps'] = ATTEMPTS
-        await message.answer("Игра начинается я загадал число от 1 до 100")
-    else:
-        await message.answer("Мы пока играем если хотите закончить напишите /cancel")
+@dp.message(F.text)
+async def filter_message(message: Message):
 
-@dp.message(F.text.lower().in_(["нет", "не хочу", "не"]))
-async def not_start_game(message: Message):
-    if not user["in_game"]:
-        await message.answer("Жаль но ладно")
-    else:
-        await message.answer("Мы сейчас играем")
+    #Если пользователя нет в списке то мы его добавляем по его id
+    if message.from_user.id not in users:
+        users[message.from_user.id] = {
+            "name": str(message.from_user.first_name),
+            "mats": 0,
+            "is_ban": False
+        }
+    #Если пользователь по какой-то причине не забанен то баним
+    if users[message.from_user.id]["is_ban"]:
+        await bot.ban_chat_member(message.chat.id, message.from_user.id)
 
-@dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 100)
-async def game(message: Message):
-    if user["in_game"]:
-        if int(message.text) == user["secret_number"]:
-            user["in_game"] = False
-            user["total_game"] += 1
-            user["win"] += 1
-            await message.answer("You Win!")
-        elif int(message.text) > user["secret_number"]:
-            user['attemps'] -= 1
-            await message.answer("Мое чило меньше")
-        elif int(message.text) < user["secret_number"]:
-            user['attemps'] -= 1
-            await message.answer("Мое чило больше")
-
-        if user["attemps"] == 0:
-            user["in_game"] = False
-            user["total_game"] += 1
-            await message.answer("You Lose!")
-    else:
-        await message.answer("Мы еще не играем")
+    #Делаем админами людей из списка
+    for admin_name_index in range(0, len(admin_name_list)):
+        if users[message.from_user.id]["name"] == admin_name_list[admin_name_index]:
+            await bot.promote_chat_member(message.chat.id, message.from_user.id, is_anonymous=True ,can_delete_messages=True, can_restrict_members=True)
+    #Баним админами людей из списка
+    for ban_user_name_index in range(0, len(ban_user_name_list)):
+        if users[message.from_user.id]["name"] == ban_user_name_list[ban_user_name_index]:
+            await bot.ban_chat_member(message.chat.id, message.from_user.id)
 
 
+    # Делим сообщение на отдельные слова
+    message_text = message.text.lower().split()
 
-@dp.message()
-async def message_our(message: Message):
-    if user["in_game"]:
-        await message.answer("Мы сейчас играем пришите числа")
-    else:
-        await message.answer("Если хотите сыграть напишите Да")
+    #Убираем пробелы из сообщения
+    full_message_text = message.text.replace(" ", "").lower()
+
+    # Проверка на маты
+    for a in range(0, len(message_text)):
+
+        for i in range(0, len(banworld_list)):
+
+            #Проверяем все слова по очереди или все слова вместе из сообщения на список матов
+            if str(message_text[a]) + "\n" == banworld_list[i] or str(full_message_text) + "\n" == banworld_list[i]:
+
+                #Если подошёл даем предупреждение
+                users[message.from_user.id]["mats"] += 1
+
+                #Костыль
+                full_message_text = str()
+
+                # Если предупреждений много баним
+                if users[message.from_user.id]["mats"] >= MAX_MATS:
+                    users[message.from_user.id]["is_ban"] = True
+                    await bot.ban_chat_member(message.chat.id, message.from_user.id)
+
+                #Пишем что выдали предупреждение
+
+                await message.answer(f"Я выдаю предуприждение пользователю {message.from_user.first_name}\nЧисло предупреждений: {users[message.from_user.id]['mats']}")
+                await message.delete()
+
+                break
+
+
+
+
+banworld.close()
+admin_name.close()
+ban_user_name.close()
 
 if __name__ == "__main__":
     dp.run_polling(bot)
