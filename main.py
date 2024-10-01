09e6f937ec7e8bf06cca082d7155f51103f7c0cd
Eldar Khanmagomedov <bratiya234@gmail.com>
2024-10-01 17:24:32 +0300

    MySQL SYSTEM!

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from aiogram.types import Message
 from aiogram import F
 from hugchat import hugchat
 from hugchat.login import Login
+from aiogram.exceptions import TelegramBadRequest
+import mysqlcodd
+
 
 BOT_TOKEN = "7921078426:AAGE9AzemlTU6XeiopTCot18tudIk27IyAg"
 bot = Bot(token=BOT_TOKEN)
@@ -20,6 +23,10 @@ ban_user_name_list = ban_user_name.readlines()
 
 MAX_MATS = 5
 
+id = None
+is_ban = None
+mats = None
+
 users = {}
 
 def AiDialog(user_input, email, passwork):
@@ -30,6 +37,7 @@ def AiDialog(user_input, email, passwork):
     chat_bot = hugchat.ChatBot(cookies=cooke.get_dict())
 
     return chat_bot.chat(user_input)
+
 
 @dp.message(Command(commands=["ai"]))
 async def Ai_progress(message: Message):
@@ -43,15 +51,27 @@ async def Ai_progress(message: Message):
 
 
 
+@dp.message(Command(commands=["unban"]))
+async def unbun(message: Message):
+    if message.from_user.first_name == "2d":
+        mysqlcodd.MySQL_UnBan(f"{message.from_user.id}")
+
+
 #Просто выдаем сколько предупреждений у пользователя
 @dp.message(Command(commands=["mats"]))
 async def mats_command(message: Message):
+
+    mats, is_ban = mysqlcodd.MySQL_REG(f"{message.from_user.id}")
+
+
     if message.from_user.id not in users:
         users[message.from_user.id] = {
             "name": message.from_user.first_name,
-            "mats": 0,
-            "is_ban": False
+            "mats": mats,
+            "is_ban": is_ban
         }
+
+
     await message.answer(f"Число пердупреждений: {users[message.from_user.id]['mats']}")
 
 
@@ -60,16 +80,23 @@ async def filter_message(message: Message):
 
     is_mat_message = False
 
+    mats, is_ban = mysqlcodd.MySQL_REG(f"{message.from_user.id}")
+
+
     #Если пользователя нет в списке то мы его добавляем по его id
-    if message.from_user.id not in users:
-        users[message.from_user.id] = {
+
+    users[message.from_user.id] = {
             "name": str(message.from_user.first_name),
-            "mats": 0,
-            "is_ban": False
+            "mats": mats,
+            "is_ban": is_ban
         }
+
     #Если пользователь по какой-то причине не забанен то баним
-    if users[message.from_user.id]["is_ban"]:
-        await bot.ban_chat_member(message.chat.id, message.from_user.id)
+    try:
+        if users[message.from_user.id]["is_ban"] == 1:
+            await bot.ban_chat_member(message.chat.id, message.from_user.id)
+    except TelegramBadRequest:
+        await message.answer("Не удается забанить пользователя так, как он админ, пожайлуста не материтесь!")
 
     #Делаем админами людей из списка
     for admin_name_index in range(0, len(admin_name_list)):
@@ -96,7 +123,9 @@ async def filter_message(message: Message):
             if str(message_text[a]) + "\n" == banworld_list[i] or str(full_message_text) + "\n" == banworld_list[i]:
 
                 #Если подошёл даем предупреждение
-                users[message.from_user.id]["mats"] += 1
+
+                mats, is_ban = mysqlcodd.MySQL_Mat(f"{message.from_user.id}")
+                users[message.from_user.id]["mats"] = mats
 
                 #Костыль
                 full_message_text = str()
@@ -104,9 +133,16 @@ async def filter_message(message: Message):
                 is_mat_message = True
 
                 # Если предупреждений много баним
-                if users[message.from_user.id]["mats"] >= MAX_MATS:
-                    users[message.from_user.id]["is_ban"] = True
-                    await bot.ban_chat_member(message.chat.id, message.from_user.id)
+                try:
+                    if users[message.from_user.id]["mats"] >= MAX_MATS:
+                        mats, is_ban = mysqlcodd.MySQL_Ban(f"{message.from_user.id}")
+
+                        users[message.from_user.id]["mats"] = mats
+                        users[message.from_user.id]["is_ban"] = is_ban
+                        await bot.ban_chat_member(message.chat.id, message.from_user.id)
+                        await message.answer(f"Пользователь: {message.from_user.first_name} забанен тк много матерился")
+                except TelegramBadRequest:
+                    await message.answer("Не удается забанить пользователя так, как он админ, пожайлуста не материтесь!")
 
                 #Пишем что выдали предупреждение
                 await message.answer(f"Я выдаю предуприждение пользователю {message.from_user.first_name}\nЧисло предупреждений: {users[message.from_user.id]['mats']}")
---
+++ mysqlcodd.py
@@ -0,0 +1,171 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+
+
+def MySQL_REG(id: str):
+
+    try:
+        with connect(
+            host="localhost",
+            user="burger",
+            password="wede12345678900",
+            database="online_move"
+        ) as connection:
+            command_table = f"SELECT id FROM mats_user WHERE id = {id}"
+            #create_table = "CREATE TABLE mats_user(id VARCHAR(100), mat INT, ban BOOL)"
+            #delete = "DROP TABLE mats_user"
+            with connection.cursor() as cursor:
+
+                cursor.execute(command_table)
+
+                id_users = cursor.fetchall()
+
+                if len(id_users) == 0 or id not in id_users[0]:
+                    create_new_user = "INSERT INTO mats_user (id, mat, ban) VALUES(%s, %s, %s)"
+                    values = [(id, 0, False)]
+
+                    cursor.executemany(create_new_user, values)
+
+                    connection.commit()
+
+                    return 0, 0
+                else:
+                    show_user_mat = f"SELECT mat, ban FROM mats_user WHERE id = {id}"
+                    cursor.execute(show_user_mat)
+
+                    result = cursor.fetchall()
+                    return result[0]
+
+    except Error as e:
+        print(e)
+
+def MySQL_Mat(id: str):
+
+    try:
+        with connect(
+            host="localhost",
+            user="burger",
+            password="wede12345678900",
+            database="online_move"
+        ) as connection:
+            create_table = f"SELECT id FROM mats_user WHERE id = {id}"
+
+            with connection.cursor() as cursor:
+
+                cursor.execute(create_table)
+
+                id_users = cursor.fetchall()
+
+                if len(id_users) == 0 or id not in id_users[0]:
+                    create_new_user = "INSERT INTO mats_user (id, mat, ban) VALUES(%s, %s, %s)"
+                    values = [(id, 1, False)]
+
+                    cursor.executemany(create_new_user, values)
+
+                    connection.commit()
+
+                    return 1, 0
+                else:
+                    show_user_mat = f"SELECT mat FROM mats_user WHERE id = {id}"
+                    cursor.execute(show_user_mat)
+
+                    mat = cursor.fetchall()
+
+                    update_mat = f"UPDATE mats_user SET mat = {mat[0][0] + 1} WHERE id = {id}"
+                    cursor.execute(update_mat)
+                    connection.commit()
+
+                    show_user_mat = f"SELECT mat, ban FROM mats_user WHERE id = {id}"
+                    cursor.execute(show_user_mat)
+
+                    result = cursor.fetchall()
+
+                    return result[0][0], result[0][1]
+
+    except Error as e:
+        print(e)
+
+
+def MySQL_Ban(id: str):
+
+    try:
+        with connect(
+            host="localhost",
+            user="burger",
+            password="wede12345678900",
+            database="online_move"
+        ) as connection:
+            create_table = f"SELECT id FROM mats_user WHERE id = {id}"
+
+            with connection.cursor() as cursor:
+
+                cursor.execute(create_table)
+
+                id_users = cursor.fetchall()
+
+                if len(id_users) == 0 or id not in id_users[0]:
+                    create_new_user = "INSERT INTO mats_user (id, mat, ban) VALUES(%s, %s, %s)"
+                    values = [(id, 5, True)]
+
+                    cursor.executemany(create_new_user, values)
+
+                    connection.commit()
+
+                    return 5, 1
+                else:
+                    update_ban = f"UPDATE mats_user SET mat = 5, ban = 1 WHERE id = {id}"
+                    cursor.execute(update_ban)
+                    connection.commit()
+
+                    show_user_mat = f"SELECT mat, ban FROM mats_user WHERE id = {id}"
+                    cursor.execute(show_user_mat)
+
+                    result = cursor.fetchall()
+
+                    return result[0][0], result[0][1]
+
+    except Error as e:
+        print(e)
+
+
+def MySQL_UnBan(id: str):
+
+    try:
+        with connect(
+            host="localhost",
+            user="burger",
+            password="wede12345678900",
+            database="online_move"
+        ) as connection:
+            create_table = f"SELECT id FROM mats_user WHERE id = {id}"
+
+            with connection.cursor() as cursor:
+
+                cursor.execute(create_table)
+
+                id_users = cursor.fetchall()
+
+                if len(id_users) == 0 or id not in id_users[0]:
+                    create_new_user = "INSERT INTO mats_user (id, mat, ban) VALUES(%s, %s, %s)"
+                    values = [(id, 0, False)]
+
+                    cursor.executemany(create_new_user, values)
+
+                    connection.commit()
+
+                    return 0, 0
+                else:
+                    update_ban = f"UPDATE mats_user SET mat = 0, ban = 0 WHERE id = {id}"
+                    cursor.execute(update_ban)
+                    connection.commit()
+
+                    show_user_mat = f"SELECT mat, ban FROM mats_user WHERE id = {id}"
+                    cursor.execute(show_user_mat)
+
+                    result = cursor.fetchall()
+
+                    return result[0][0], result[0][1]
+
+    except Error as e:
+        print(e)
